550beb288dcba745d5271ccaddd064e5db28b9ef
kevin.huang <kevin01.huang@vipshop.com>
2016-03-01 16:38:44 +0800

    添加相关配置

---
+++ src/com/Test.java
@@ -0,0 +1,13 @@
+package src.com;
+
+/**
+ * @version 0.0.1-SNAPSHOT
+ * @author kevin01.huang
+ *
+ */
+public class Test {
+
+	public static void main(String[] args){
+		System.out.println("...................");
+	}
+}
